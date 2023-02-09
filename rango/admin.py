c92369e74cfb74b58865f06036088dc45adc7df3
Ferdinand <2668241Z@student.gla.ac.uk>
2023-02-09 23:29:31 +0000

    Chapter 6/7, not done

--- rango/admin.py
+++ rango/admin.py
@@ -4,7 +4,8 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Page, PageAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
